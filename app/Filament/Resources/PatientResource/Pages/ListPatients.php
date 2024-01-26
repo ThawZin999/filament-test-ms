8f322e4ae59932239d652540f9c9881c14fba714
ThawZin999 <egthawzin@gmail.com>
2024-01-26 20:51:53 +0630

    Tested

---
+++ app/Filament/Resources/PatientResource/Pages/ListPatients.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Resources\PatientResource\Pages;
+
+use App\Filament\Resources\PatientResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Database\Eloquent\Builder;
+
+class ListPatients extends ListRecords
+{
+    protected static string $resource = PatientResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
